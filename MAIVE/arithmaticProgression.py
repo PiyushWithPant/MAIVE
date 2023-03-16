b6130b6c907ca26bc6cdfe39bd50a8bd92dda4cf
Piyush Pant <piyushpant15@gmail.com>
2023-03-17 00:15:56 +0530

    AP module

--- MAIVE/arithmaticProgression.py
+++ MAIVE/arithmaticProgression.py
@@ -12,9 +12,13 @@ class ArithmaticProgression():
         if(len(kwargs) != 0):
             
             try:
+                if(len(kwargs) > 3):
+                    raise Exception("Please pass only 'a', 'd' and 'n'.")
 
                 # if(kwargs["a"] and kwargs["d"] and kwargs["n"]) in kwargs:
                 if "a" in kwargs and "d" in kwargs and "n" in kwargs:
+                    
+                    
 
                     if((type(kwargs["a"]) is int or type(kwargs["a"]) is float) and (type(kwargs["d"]) is int or type(kwargs["d"]) is float) and (type(kwargs["n"]) is int or type(kwargs["n"]) is float) ):
                         
@@ -26,10 +30,7 @@ class ArithmaticProgression():
                             d = kwargs["d"]
                             n = kwargs["n"]
                             
-                            for i in range(1,n+1):
-                
-                                element = a + (i-1)*d
-                                self.AP.append(element)
+                            self.AP = [a + (i-1)*d for i in range(1, n+1)]
                         
                             return self.AP
                         
@@ -51,16 +52,20 @@ class ArithmaticProgression():
             except TypeError as e:
                 return e
             
+            except Exception as e:
+                return e
 
         else:
             a = random.randint(-101,101)
             d = random.randint(-101,101)
             n = 10
             
+            self.AP = [a + (i-1)*d for i in range(1, n+1)]   # AP formula
             
-            for i in range(1, n+1):
-                element = a + (i-1)*d   # AP formula
-                self.AP.append(element)
+            # for i in range(1, n+1):
+            #     element = a + (i-1)*d   # AP formula
+                
+            #     # self.AP.append(element)
                 
             return self.AP
                 
@@ -69,8 +74,61 @@ class ArithmaticProgression():
 
         
     
-    def AP_checkIfAP(self):
-        pass
+    def AP_checkIfAP(self, list=[]):
+        """Function to check if the LIST is an Arithmatic Progression or not. Returns TRUE or FALSE"""
+        
+        try:
+            if (len(list) != 0):
+                if(len(list) >= 3):
+                    
+                    a = list[0]
+                    n = len(list)
+                    d = list[1] - a # this should be constant throughout
+                    
+                    for i in range(2, n): # starting with 3rd element since a, a+d, a+2d and then n-1= (3-1)d = 2d
+                        element = a + (i)*d 
+                        
+                        if(list[i] != element):
+                            return False
+                    # if the for loop fully executed
+                    return True
+                    
+                    # diff = list[1] - a
+                    
+                    # diff_test = 0
+                    
+                    
+                    
+                    # for element in list:
+                            
+                    #     # to check if all the elements are int or float
+                        
+                    #     if(type(element) is not int) and (type(element) is not float):
+                    #         raise Exception("Please provide INTEGER or FLOAT datatype in the list only.")
+
+                    #     # checking if AP
+                        
+                    #     if element == a:
+                    #         continue
+                        
+                    #     diff_test = element - a
+                        
+                    #     if (diff == diff_test):
+                    #         d = diff_test
+                    #         return True # an AP
+                    #     else:
+                    #         return False # not an AP
+
+                else:
+                    raise Exception("List should have at least 3 or more elements to check for Arithmatic progression.")
+                    
+            else:
+                raise Exception("LIST not found! Please input a list.")
+            
+        
+        
+        except Exception as e:
+            return e
     
     
     def AP_findNthElement(self):
--- MAIVE/testing.py
+++ MAIVE/testing.py
@@ -4,7 +4,8 @@ from index import Maive as M
 
 maive = M()
 
-test = maive.AP_checkIfAP()
+test = maive.AP_checkIfAP([-10, -5, 0, 5, 10])
+
 
 print(test)
 

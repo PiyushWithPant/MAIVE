42d29d672662accd0a57799e914c97c2fe596a04
Piyush Pant <piyushpant15@gmail.com>
2023-03-30 23:37:16 +0530

    PB

--- MAIVE/probability.py
+++ MAIVE/probability.py
@@ -5,15 +5,39 @@ class Probability:
         self.probability = 0
         
     def P_probability(self, **kwargs):
+        """
+        To find the probability of an event,\n
+        s = sample space \n
+        e = favourable event \n
+        """
+        
         try:
             
             if(not len(kwargs) == 0):
-            
-                for (key,value) in kwargs.items():
-                    print(value)
+                
+                
+                
                 s = kwargs['s']
                 e = kwargs['e']
-                self.probability = s/e
+                
+            
+                for (key,value) in kwargs.items():  # to check data type of each value in the kwargs
+                    
+                    if(type(value) != int and type(value) != float):
+                        raise Exception("Please enter only INPUT or FLOAT value!")
+                    
+                    
+                # if( s < e): # checking if sample space is greater than e
+                #     raise Exception("The sample space cannot be smaller than the favourable event!")
+               
+                # basic probability
+               
+                self.probability = round(e/s, 2)
+                
+                
+                if(self.probability > 1 or self.probability < 0):
+                    raise Exception("Invalid input!")
+                
                 return self.probability
             
             else:
--- MAIVE/testing.py
+++ MAIVE/testing.py
@@ -4,7 +4,7 @@ from index import Maive as M
 
 maive = M()
 
-test = maive.P_probability(s=100, e=10)
+test = maive.P_probability(s=10, e=9)
 
 print(test)
 # print(float(test["alpha"]))

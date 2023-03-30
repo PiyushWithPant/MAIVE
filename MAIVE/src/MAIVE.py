d47b452e9fdaf50645048513a7e0ed2c683705ca
Piyush Pant <piyushpant15@gmail.com>
2023-03-31 00:28:37 +0530

    PB

--- MAIVE/src/MAIVE.py
+++ MAIVE/src/MAIVE.py
@@ -2,6 +2,7 @@ from arithmaticProgression import ArithmaticProgression
 from quadraticEquation import QuadraticEquation
 from probability import Probability
 
+
 class Maive(ArithmaticProgression, QuadraticEquation, Probability):
     
     def __init__(self):

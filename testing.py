7641fe6650db246055db3f23123fe25adc93bcae
Piyush Pant <piyushpant15@gmail.com>
2023-04-05 00:48:02 +0530

    PB

--- testing.py
+++ testing.py
@@ -8,7 +8,7 @@ from maive.maive import Maive
 
 m = Maive()
 
-test = m.P_isIndependentEvent(a = [3,4,2],b=[3,4,5], pa = 0.5, pb = 0.5)
+test = m.P_isIndependentEvent(pab=1/4, pa = 0.5, pb = 0.5)
 
 print(test)
 
